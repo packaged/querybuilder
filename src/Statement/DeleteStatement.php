b275dbf8996f9fa7848e6228bba80811e48e186c
Tom Kay <tom.kay@justdevelop.it>
2014-12-08 18:21:25 +0000

    include WhereTrait for DeleteStatement and UpdateStatement

--- src/Statement/DeleteStatement.php
+++ src/Statement/DeleteStatement.php
@@ -1,8 +1,12 @@
 <?php
 namespace Packaged\QueryBuilder\Statement;
 
+use Packaged\QueryBuilder\Builder\Traits\WhereTrait;
+
 class DeleteStatement extends AbstractStatement
 {
+  use WhereTrait;
+
   protected function _getOrder()
   {
     return ['DELETEFROM', 'WHERE'];
--- src/Statement/UpdateStatement.php
+++ src/Statement/UpdateStatement.php
@@ -1,8 +1,12 @@
 <?php
 namespace Packaged\QueryBuilder\Statement;
 
+use Packaged\QueryBuilder\Builder\Traits\WhereTrait;
+
 class UpdateStatement extends AbstractStatement
 {
+  use WhereTrait;
+
   protected function _getOrder()
   {
     return ['UPDATE', 'SET', 'WHERE'];
