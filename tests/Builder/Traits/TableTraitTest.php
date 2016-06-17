79bb9c2f7acf53b83c658b8bcf404859b85a018f
Brooke Bryan <brooke@bajb.net>
2016-06-17 17:05:44 +0100

    Table Trait Test

---
+++ tests/Builder/Traits/TableTraitTest.php
@@ -0,0 +1,34 @@
+<?php
+namespace Packaged\Tests\QueryBuilder\Builder\Traits;
+
+use Packaged\QueryBuilder\Expression\TableExpression;
+use Packaged\QueryBuilder\Expression\Traits\TableTrait;
+use Packaged\QueryBuilder\SelectExpression\TableSelectExpression;
+use Packaged\QueryBuilder\Statement\AbstractStatement;
+
+class TableTraitTest extends \PHPUnit_Framework_TestCase
+{
+  public function testGetTable()
+  {
+    $statement = new FinalTableTrait();
+    $statement->setTable(null);
+    $this->assertNull($statement->getTable());
+
+    $table = TableSelectExpression::create('table_one');
+    $table->setAlias('t1');
+
+    $statement = new FinalTableTrait();
+    $statement->setTable($table);
+    $this->assertInstanceOf(TableExpression::class, $statement->getTable());
+  }
+}
+
+class FinalTableTrait extends AbstractStatement
+{
+  use TableTrait;
+
+  protected function _getOrder()
+  {
+    return ["FROM"];
+  }
+}
